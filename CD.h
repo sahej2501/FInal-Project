b44ab88f5cf53d2ca28d6a46e64ae521f7f4395b
JayNuet <jaynuet@outlook.com>
2020-12-01 13:16:00 -0600

    Create CD.h

---
+++ CD.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "accounts.h"
+using namespace std;
+
+class CD : public accounts
+{
+private:
+    string status;
+    double penalty;
+
+public:
+    //Default constructor
+    CD() : accounts()
+    {
+        annualInterestRate = 0;//whatever fixed amnt we decide
+    }
+
+    void deposit(double amount)
+    {
+        if(open == true)
+        {
+            accountBalance += amount;
+            cout << "A $" << amount << " deposit has been made" << endl;
+        }
+        else
+        {
+            cout << "Account not open!";
+        }
+    }
+
+    double withdraw(double amount)
+    {
+        if (open == true)
+        {
+            accountBalance -= amount;
+            cout << "withdraw Successful!" << endl;
+        }
+        else
+        {
+            cout << "Account not open!";
+        }
+        return accountBalance;
+    }
+
+    void cancellation()
+    {
+        accountBalance -= penalty;
+        withdraw(accountBalance);
+    }
+};
