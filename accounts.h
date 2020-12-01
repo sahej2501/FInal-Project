c7d482500d8f59199ed2489f45474f2ef6111f8c
mgegg016 <72768427+mgegg016@users.noreply.github.com>
2020-12-01 13:11:03 -0600

    acounts 1.1

--- accounts.h
+++ accounts.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cmath>
 using namespace std;
 
 class accounts{
-    private:
+    protected:
         string accountNumber;
         double accountBalance;
         bool open; //bool to see if the account is accessible 
@@ -14,11 +17,17 @@ class accounts{
         string username;
         string password;
         string timeOpened;
+        const int monthDays[12]= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         double maturity;
         bool closed;
+        struct Date{
+            int d,m,y;
+        };
+
+
     public:
         //contructor
-        accounts(string aN="0000000000", double aB=0.0, bool o=false, double aI=0.0, double aS=0.0, string aT="0001", string uN = "N/A", string pW = "N/A",double mat=0.0, bool close=false){
+        accounts(string aN="0000000000", double aB=0.0, bool o=true, double aI=0.0, double aS=0.0, string aT="0001", string uN = "N/A", string pW = "N/A",double mat=0.0, bool close=false){
             //default Number is 0000000, account Balance is 0, its closed, 0.0 interest rate, 0.0 annual charge
             //it starts at a low risk, it has N for none account type, and 0 annual service charge.
             accountNumber=aN;
@@ -98,7 +107,7 @@ class accounts{
         }
 
 
-        virtual double withdrawl(double amount){
+        double withdrawl(double amount){
             if(open){
                 if(amount>accountBalance){
                     cout<<"Withdrawl unsuccessful, insufficient amount!";
@@ -114,7 +123,7 @@ class accounts{
             return accountBalance;
         }
 
-        virtual void deposit(double amount){
+        void deposit(double amount){
             if(open){
                 accountBalance+=amount;
                 cout<<"Deposit successful! Current balance: "<< accountBalance;
@@ -123,96 +132,64 @@ class accounts{
                 cout<<"Account not open!";
             }
         }
-};
 
-//paste jay stuff
-class Checking : public accounts
-{
-public:
-    //Default constructor
-    Checking() : accounts(){}
+    Date getCurrentTime(){
+        time_t now = time(0);
+        Date tempDate;
+        vector <Date> storedDates;
+        tm *ltm = localtime(&now);
+        int year= 1900 + ltm->tm_year;
+        int month= 1 + ltm->tm_mon;
+        int day= ltm->tm_mday;
+        tempDate.y=year;
+        tempDate.m=month;
+        tempDate.d=day;
 
-    //withdraw function
-    double withdraw(double amount)
-    {
-        double fundCharge = -25.0;
-        if (open == true)
-        {
-            if (accountBalance - amount < 0)   //if the withdrawal amount will cause a negative, just deduct the $25 fundCharge from the balance.
-            {
-                accountBalance = fundCharge;
-                cout << "You withdrew more than you had, fund charge has been applied!" << endl;
-            }
-            else
-            {
-                accountBalance -= amount;
-                cout << "withdraw Successful!" << endl;
-            }
-        }
-        else
-        {
-            cout << "Account not open!";
-        }
-        return accountBalance;
+
+        return tempDate;
     }
 
-    void deposit(double amount)   //a deposit function
-    {
-        if(open == true)
-        {
-            accountBalance += amount;
-            cout << "A $" << amount << " deposit has been made" << endl;
-        }
-        else
-        {
-            cout << "Account not open!";
-        }
-    }
-};
+    int countLeapYears(Date d) { 
+        int years = d.y; 
+  
+        // Check if the current year needs to be considered 
+        // for the count of leap years or not 
+        if (d.m <= 2) 
+            years--; 
+  
+        // An year is a leap year if it is a multiple of 4, 
+        // multiple of 400 and not a multiple of 100. 
+        return years / 4 - years / 100 + years / 400; 
+    } 
 
-class CD : public accounts
-{
-private:
-    string status;
-    double penalty;
+    int getDifference(Date dt1, Date dt2) { 
+    // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1' 
+  
+    // initialize count using years and day 
+        long int n1 = dt1.y*365 + dt1.d; 
+  
+    // Add days for months in given date 
+        for (int i=0; i<dt1.m - 1; i++) 
+            n1 += monthDays[i]; 
+  
+    // Since every leap year is of 366 days, 
+    // Add a day for every leap year 
+        n1 += countLeapYears(dt1); 
+  
+    // SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2' 
+  
+        long int n2 = dt2.y*365 + dt2.d; 
+        for (int i=0; i<dt2.m - 1; i++) 
+            n2 += monthDays[i]; 
+        n2 += countLeapYears(dt2); 
+  
+    // return difference between two counts 
+        return (n2 - n1); 
+    } 
 
-public:
-    //Default constructor
-    CD() : accounts()
-    {
-        annualInterestRate = 0;//whatever fixed amnt we decide
-    }
-
-    void deposit(double amount)
-    {
-        if(open == true)
-        {
-            accountBalance += amount;
-            cout << "A $" << amount << " deposit has been made" << endl;
-        }
-        else
-        {
-            cout << "Account not open!";
-        }
-    }
-
-    double withdraw(double amount)
-    {
-        if (open == true)
-        {
-            accountBalance -= amount;
-            cout << "withdraw Successful!" << endl;
-        }
-        else
-        {
-            cout << "Account not open!";
-        }
-        return accountBalance;
+    string formatDate(Date date){
+        string formattedDate= "d/"+to_string(date.d)+"/"+to_string(date.m)+"/"+to_string(date.y);
+        return formattedDate;
     }
 
-    void cancellation()
-    {
-        accountBalance -= penalty;
-        withdraw(accountBalance);
-    }
 };
