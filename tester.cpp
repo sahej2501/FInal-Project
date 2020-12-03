2687a49625273d6b4eed36baae0b3161650cefd9
mgegg016 <72768427+mgegg016@users.noreply.github.com>
2020-12-03 17:14:20 -0600

    1.3

--- tester.cpp
+++ tester.cpp
@@ -1,27 +1,16 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <vector>
-#include <ctime>
-#include <cmath>
 #include "timeHandler.h"
 #include "directoryHandler.h"
 using namespace std;
 
-int main(){
-    timeHandler t1;
-    directory dir;
 
-    cout<<"Date 1:"<<t1.formatDate(t1.getCurrentTime())<<endl;
-    chdir(dir.basePath.data());
-    dir.createFiles("ae87");
-    dir.writeToFile("ae87",'t',"+$1000 d/1/12/2020");
-    timeHandler::Date d1;
-    timeHandler::Date d2;
-    d1=t1.getCurrentTime();
-    d2.d=2;
-    d2.m=12;
-    d2.y=2020;
-    cout<<"Date 2: "<<t1.formatDate(d2)<<endl;
-    cout<<"Difference between date 1 and date 2: "<<t1.getDifference(d1,d2)<<endl;
-    return 0;
+int main(){
+    timeHandler t;
+    directory d;
+
+    cout<<d.getCurrentDir()<<endl;
+    d.createFiles("mike1");
+    d.writeToFile("mike1",'t',"scb");
 }
