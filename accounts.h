6a9c7f5a91ece271fbe412476c2357571b960003
mgegg016 <72768427+mgegg016@users.noreply.github.com>
2020-12-04 19:12:56 -0600

    removed time functions from accounts

--- accounts.h
+++ accounts.h
@@ -2,6 +2,7 @@
 #define ACCOUNTS_H
 #include <string>
 #include <iostream>
+#include "timeHandler.h"
 #include <vector>
 #include <ctime>
 #include <cmath>
@@ -18,12 +19,8 @@ class accounts{
         string username;
         string password;
         string timeOpened;
-        const int monthDays[12]= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int maturity;
         bool closed;
-        struct Date{
-            int d,m,y;
-        };
 
 
         Date openedDate;
@@ -142,64 +139,6 @@ class accounts{
         }
 
 
-        Date getCurrentTime(){
-            time_t now = time(0);
-            Date tempDate;
-            vector <Date> storedDates;
-            tm *ltm = localtime(&now);
-            int year= 1900 + ltm->tm_year;
-            int month= 1 + ltm->tm_mon;
-            int day= ltm->tm_mday;
-            tempDate.y=year;
-            tempDate.m=month;
-            tempDate.d=day;
-
-
-            return tempDate;
-        }
-
-        int countLeapYears(Date d) { 
-            int years = d.y; 
-  
-            // Check if the current year needs to be considered 
-            // for the count of leap years or not 
-            if (d.m <= 2) 
-                years--; 
-  
-            // An year is a leap year if it is a multiple of 4, 
-            // multiple of 400 and not a multiple of 100. 
-            return years / 4 - years / 100 + years / 400; 
-        } 
-
-        int getDifference(Date dt1, Date dt2) { 
-        // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1' 
-  
-        // initialize count using years and day 
-            long int n1 = dt1.y*365 + dt1.d; 
-  
-        // Add days for months in given date 
-            for (int i=0; i<dt1.m - 1; i++) 
-                n1 += monthDays[i]; 
-  
-        // Since every leap year is of 366 days, 
-        // Add a day for every leap year 
-            n1 += countLeapYears(dt1); 
-  
-        // SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2' 
-  
-            long int n2 = dt2.y*365 + dt2.d; 
-            for (int i=0; i<dt2.m - 1; i++) 
-                n2 += monthDays[i]; 
-            n2 += countLeapYears(dt2); 
-  
-        // return difference between two counts 
-            return (n2 - n1); 
-        } 
-
-        string formatDate(Date date){
-            string formattedDate= "d/"+to_string(date.d)+"/"+to_string(date.m)+"/"+to_string(date.y);
-            return formattedDate;
-        }
 
     void yearsPassed(){ //gets the difference of the two dates and if its greater than 365 it returns the difference of the amount of years to maturity as an int
     // for jay
