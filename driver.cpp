a71d9196270361c63f133f54c47f66a418a0effa
sahej2501 <bhatia.sahejbir.01@gmail.comgit config --global user.name Sahej>
2020-12-06 18:33:07 -0600

    Driver with Sahej Changes

--- driver.cpp
+++ driver.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <string>
 #include<fstream>
 #include "AccountNode.h"
 #include "Bank.h"
@@ -29,6 +30,12 @@ vector<Official> officialsVec;
 vector<Admin> adminsVec;
 vector<User> usersVec;
 
+//Added by Sahej
+BankTree tree;
+int key;
+
+directory d;
+
 void createOfficial(string, string, vector<Official> &officialsVec);
 void createUser(string, string, string, vector<User> &usersVec);
 bool LoginPasswordMatch(string, string, vector<User> &usersVec);
@@ -40,6 +47,11 @@ void readOfficialLogins(vector<Official> &officialsVec);
 void readUserLogins(vector<User> &usersVec);
 void writeToLogins(vector<Admin> &adminsVec, vector<Official> &officialsVec, vector<User> &usersVec);
 void changePassword(string, string, vector<User> &usersVec);
+
+//Sahej import
+void depOrWidth();
+void openAccount(AllAccounts &a);
+string generateAC(int keyPassed);
 
 
 int main()
@@ -327,3 +339,240 @@ void changePassword(string id, string newPswd, vector<User> &usersVec)
     if(present == false)
         cout << "user's profile does not exist" << endl;
 }
+
+//Added by Sahej
+void depOrWidth()
+{
+    AllAccounts currAcount;
+    string login, password, accountNum;
+    bool run = false;
+
+    while(!run)
+    {
+        cout<<"Please enter Login for User: "<<endl;
+        cin>>login;
+        cout<<"Please enter user Password: "<<endl;
+        cin>>password;
+        for(int i = 0; i < usersVec.size(); i++)
+        {
+            if(login == usersVec[i].username) //&& password == usersVec[i].password)
+            {
+                if(password == usersVec[i].password)
+                {
+                    accountNum = usersVec[i].accntNum;
+                    run = true;
+                }
+            }
+            else
+            {
+                run = false;
+            }
+        }
+        if(!run)
+        {
+            cout<<"No login or password match, try again"<<endl;
+        }
+    }
+
+    int tempKey = d.getKey(accountNum);
+    //cout<<tempKey<<" "<<accountNum<<endl;
+    tree.searchAcc(tempKey);
+    //cout<<tree.currAccount.acc->getName()<<endl;
+    tree.modifi(currAcount);
+    cout<<currAcount.getAccountNumber()<<endl;
+    
+
+    bool userRun = true;
+    while(userRun)
+    {
+        int choice;
+        cout<<"[1] Savings"<<'\n'<<"[2] Checkings"<<'\n'<<"[3] CD"<<'\n'<<"[4] Quit"<<endl;
+        cin>>choice;
+        switch (choice)
+        {
+        case 1:
+            cin.ignore();
+            int depChoice; 
+            cout<<"[1] Deposit"<<'\n'<<"[2] Withdraw"<<'\n'<<
+            "[3] Check Balance"<<'\n'<<"[4] Back"<<endl;
+            cin>>depChoice;
+            switch (depChoice)
+            {
+            case 1:
+                cin.ignore();
+                int dep;
+                cout<<"Enter deposit ammount"<<endl;
+                cin>>dep;
+                currAcount.savingsDeposit(dep);
+                cout<<"Current Balance is: "<<currAcount.getSavingsBalance()<<endl;
+                break;
+            case 2:
+                cin.ignore();
+                //int dep;
+                cout<<"Enter widthdraw ammount"<<endl;
+                cin>>dep;
+                currAcount.savingsWithdraw(dep);
+                cout<<"Current Balance is: "<<currAcount.getSavingsBalance()<<endl;
+                break;
+            case 3:
+                cin.ignore();
+                cout<<"Current Balance is: "<<currAcount.getSavingsBalance()<<endl;
+                break;
+            case 4:
+                break;
+            default:
+                cout<<"Invalid Input, try again"<<endl;
+                break;
+            }
+            break;
+        case 2:
+            cin.ignore();
+            //int depChoice; 
+            cout<<"[1] Deposit"<<'\n'<<"[2] Withdraw"<<'\n'<<
+            "[3] Check Balance"<<'\n'<<"[4] Back"<<endl;
+            cin>>depChoice;
+            switch (depChoice)
+            {
+            case 1:
+                cin.ignore();
+                int dep;
+                cout<<"Enter deposit ammount"<<endl;
+                cin>>dep;
+                currAcount.checkingDeposit(dep);
+                cout<<"Current Balance is: "<<currAcount.getCheckingBalance()<<endl;
+                break;
+            case 2:
+                cin.ignore();
+                //int dep;
+                cout<<"Enter widthdraw ammount"<<endl;
+                cin>>dep;
+                currAcount.checkingWithdraw(dep);
+                cout<<"Current Balance is: "<<currAcount.getCheckingBalance()<<endl;
+                break;
+            case 3:
+                cin.ignore();
+                cout<<"Current Balance is: "<<currAcount.getCheckingBalance()<<endl;
+                break;
+            case 4:
+                break;
+            default:
+                cout<<"Invalid Input, try again"<<endl;
+                break;
+            }
+            break;
+        case 3:
+            cin.ignore();
+            //int depChoice; 
+            cout<<"[1] Deposit"<<'\n'<<"[2] Withdraw"<<'\n'<<
+            "[3] Check Balance"<<'\n'<<"[4] Cancel CD"<<'\n'<<"[5] Back"<<endl;
+            cin>>depChoice;
+            switch (depChoice)
+            {
+            case 1:
+                cin.ignore();
+                int dep;
+                cout<<"Enter deposit ammount"<<endl;
+                cin>>dep;
+                currAcount.CDDeposit(dep);
+                cout<<"Current Balance is: "<<currAcount.getCDBalance()<<endl;
+                break;
+            case 2:
+                cin.ignore();
+                //int dep;
+                cout<<"Enter widthdraw ammount"<<endl;
+                cin>>dep;
+                currAcount.CDWithdraw(dep);
+                cout<<"Current Balance is: "<<currAcount.getCDBalance()<<endl;
+                break;
+            case 3:
+                cin.ignore();
+                cout<<"Current Balance is: "<<currAcount.getCDBalance()<<endl;
+                break;
+            case 4:
+                cin.ignore();
+                currAcount.cdCancel();
+                break;
+            case 5:
+                break;
+            default:
+                cout<<"Invalid Input, try again"<<endl;
+                break;
+            }
+            break;
+        case 4:
+            userRun = false;
+            break;
+        default:
+            cout<<"Invalid Input, try again"<<endl;
+            break;
+            }
+
+    }
+    tree.upadteAcc(currAcount.getKey(), currAcount);
+    tree.searchAcc(currAcount.getKey());
+}
+
+void openAccount(AllAccounts &a)
+{
+    string accNum, login, password, name, firstName, lastName, phone, address;
+
+    //New login and password for user
+    bool run = true;
+    while(run)
+    {
+        cout<<"Please choose a login Username: "<<endl;
+        getline(cin, login);
+        cout<<"Please choose a password: "<<endl;
+        getline(cin, password);
+        run = LoginPasswordMatch(login, password, usersVec);
+        if(run)
+        {
+            cout<<"Login or Password already exsist, try again"<<endl;
+        }
+    }
+    
+    //generating a new account num
+    accNum = generateAC(key);
+    a.setKey(d.getKey(accNum));
+    a.setAccountNumber(accNum);
+
+    //Entering Name for new user
+    cin.clear();
+    cout<<"Please Enter Your Full Name"<<endl;
+    getline(cin, name);
+    firstName = name.substr(0, name.find(" "));
+    lastName = name.substr(name.find(" "), name.length());
+    a.setFirstLastName(firstName, lastName);
+    //cin.ignore();
+
+    // //Entering Phone number
+    cin.clear();
+    cout<<"Please Enter a Phone Number for the Account (format: xxx-xxx-xxxx): "<<endl;
+    getline(cin, phone);
+    a.setPhone(phone);
+    //cin.ignore();
+
+    //Entering Address
+    cin.clear();
+    cout<<"Enter a Address for the account: "<<endl;
+    getline(cin, address);
+    a.setAddress(address);
+
+    tree.insertAcc(a);
+    //tree.searchAcc(a.getKey());
+    createUser(login, password, accNum, usersVec);
+}
+
+string generateAC(int keyPassed){
+    srand(time(0));
+    const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int len = sizeof(alphanum) - 1;
+    string actN;
+    actN+=to_string(keyPassed);
+    for(int i =0; i<=10;i++){
+        actN+=alphanum[rand()%len];
+    }
+    key++;
+    return actN;
+}
+//Added by Sahej
